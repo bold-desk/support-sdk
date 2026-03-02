b9ed33607a05f855390514ea581d51afee18e2e2
SomaPrasanna4037 <somaprasanna.m@syncfusion.com>
2026-03-02 12:05:18 +0530

    Changes for the icon in samples

--- bolddesksdk_react_native_sample/index.js
+++ bolddesksdk_react_native_sample/index.js
@@ -2,16 +2,17 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { Image, AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import messaging from '@react-native-firebase/messaging';
 import { BoldDeskSupportSDK} from 'bolddesk_support_sdk';
+import { IMAGES } from "../constants/constants";
 
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   try {
-    BoldDeskSupportSDK.showNotification("sample_app_logo",remoteMessage.data)
+    BoldDeskSupportSDK.showNotification(IMAGES.BOLDDESK_LOGO,remoteMessage.data)
     console.log('[FCM Background Handler] Notification shown successfully');
   } catch (error) {
     console.error('[FCM Background Handler] Error showing notification:', error);
